bb230556a2a69c97ad08f7987fefc94825cf81a1
Maximo Dominguez <mamodom@gmail.com>
2018-06-24 17:03:34 -0400

    Adding a stack navigator

--- App.js
+++ App.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Constants } from 'expo';
+import { createStackNavigator } from 'react-navigation';
 
 import DeckList from './components/DeckList';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={{ height: Constants.statusBarHeight }} />
-        <DeckList />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+export default createStackNavigator({
+  Home: {
+    screen: DeckList,
   },
 });
