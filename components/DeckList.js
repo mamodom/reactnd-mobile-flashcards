62d7c47523cc14ff380648a93c09e41b00ff95c3
Maximo Dominguez <mamodom@gmail.com>
2018-06-24 16:41:21 -0400

    Solving key warning

--- components/DeckList.js
+++ components/DeckList.js
@@ -30,16 +30,16 @@ const DeckList = () => (
   >
     <FlatList
       data={[
-        { name: 'History', cardCount: 1 },
-        { name: 'Physics', cardCount: 2 },
-        { name: 'Maths', cardCount: 3 },
-        { name: 'Chemestry', cardCount: 1 },
-        { name: 'Biology', cardCount: 5 },
-        { name: 'French', cardCount: 2 },
-        { name: 'Spanish', cardCount: 0 },
-        { name: 'Geography', cardCount: 3 },
+        { key: 'History', name: 'History', cardCount: 1 },
+        { key: 'Physics', name: 'Physics', cardCount: 2 },
+        { key: 'Maths', name: 'Maths', cardCount: 3 },
+        { key: 'Chemestry', name: 'Chemestry', cardCount: 1 },
+        { key: 'Biology', name: 'Biology', cardCount: 5 },
+        { key: 'French', name: 'French', cardCount: 2 },
+        { key: 'Spanish', name: 'Spanish', cardCount: 0 },
+        { key: 'Geography', name: 'Geography', cardCount: 3 },
       ]}
-      renderItem={p => <DeckListItem key={p.item.name} {...p.item} />}
+      renderItem={p => <DeckListItem {...p.item} />}
     />
   </View>
 );
