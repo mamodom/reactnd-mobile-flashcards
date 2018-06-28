64155d2573a6f42e44ea563432e60360276d0fa6
Maximo Dominguez <mamodom@gmail.com>
2018-06-28 19:29:01 -0400

    This shouldn't be tracked

--- build/App.js
+++
@@ -1,8 +0,0 @@
-import { createStackNavigator } from 'react-navigation';
-import DeckList from './components/DeckList';
-export default createStackNavigator({
-    Home: {
-        screen: DeckList,
-    },
-});
-//# sourceMappingURL=App.js.map
